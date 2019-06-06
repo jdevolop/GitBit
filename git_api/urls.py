2e416a243b66271612eb0d71a30b22b0dd348712
Jamshid <rabbimov5005@gmail.com>
2019-06-06 23:58:28 +0500

    csv ga route qoshilgan

--- git_api/urls.py
+++ git_api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from django.conf.urls import url
-from .middlewares import search, commit
+from .middlewares import search, commit, download_as_csv
 
 urlpatterns = [
     path('search/', search, name='search'),
-    url(r'^commit/(?P<owner>.+)/(?P<repo>.+)/$', commit, name='commit')
+    url(r'^commit/(?P<owner>.+)/(?P<repo>.+)/$', commit, name='commit'),
+    path('download/csv', download_as_csv, name='download')
 ]

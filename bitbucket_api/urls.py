ce3f267066c03981eba29663677d90024a0ef95f
Jamshid <rabbimov5005@gmail.com>
2019-06-06 23:54:28 +0500

    csv ga url qoshilgan

--- bitbucket_api/urls.py
+++ bitbucket_api/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
 
-from .middlewares import search, commit
+from .middlewares import search, commit, download_as_csv
 
 
 
 urlpatterns = [
     url(r'^search/(?P<un>.+)/repos/$', search, name='bit_search'),
-    url(r'^commit/(?P<un>.+)/(?P<slug>.+)/$', commit, name='last_commit'),   
+    url(r'^commit/(?P<un>.+)/(?P<slug>.+)/$', commit, name='last_commit'), 
+    url(r'^download/csv', download_as_csv, name='download'),  
 ]
 

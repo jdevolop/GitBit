508a743d63838cf2f0e280e7bbe231b53f02b995
Jamshid <rabbimov5005@gmail.com>
2019-06-06 23:58:01 +0500

    csv ni yuklash qoshilgan

--- git_api/middlewares.py
+++ git_api/middlewares.py
@@ -1,6 +1,6 @@
-from django.http import HttpResponse
-import requests, json
-from .views import repo_parser, commit_filter, d_filter
+from django.http import HttpResponse, HttpResponseNotFound
+import requests, json, datetime, csv
+from .views import repo_parser, commit_filter
 
 def search(request):
     term = request.GET['search_term']
@@ -14,19 +14,34 @@ def search(request):
 
 def commit(request, owner, repo):
     r = requests.get('https://api.github.com/repos/{owner}/{repo}/commits'.format(owner=owner, repo=repo))
-    s = r.json()[:1][0]
-    a = commit_filter(s)
     
-    res = dict()
-    res['message'] = a['commit']['message']
-    res['hash'] = a['sha']
-    res['committer_date'] = a['commit']['committer']['date']
-    res['url'] = a['html_url']
-    required_fields = ['login', 'avatar_url', 'html_url']
-    res['committer_info'] = d_filter(a['committer'], required_fields)
+    
+    if (isinstance(r.json(), dict) == True):
+        a = commit_filter(empty=True)
+    else:
+        s = r.json()[:1][0]
+        a = commit_filter(s)
 
-    resp = HttpResponse(json.dumps(res))
+    resp = HttpResponse(json.dumps(a))
     resp['Content-Type'] = 'application/json; charset=utf-8'
     resp['Access-Control-Allow-Origin'] = '*'
     return resp
 
+def download_as_csv(request):
+    search_term = request.GET.get('search_term')
+    req = requests.get('http://127.0.0.1/github/api/search/?search_term='+search_term)
+    
+    if req.status_code == 200:        
+        data = req.json()['data']
+        resp = HttpResponse(content_type='text/csv')
+        resp['Content-Disposition'] = f'attachment; filename="result_github_{datetime.datetime.now():%Y-%m-%d}.csv"'
+
+        w = csv.writer(resp)
+        w.writerow(['full_name', 'html_url', 'description', 'updated_at', 'stargazers_count', 'language'])
+
+        for repo in data:
+            w.writerow([repo['full_name'], repo['html_url'], repo['description'], repo['updated_at'], repo['stargazers_count'], repo['language']])
+
+        return resp
+    else:
+        return HttpResponseNotFound('Not Found')
